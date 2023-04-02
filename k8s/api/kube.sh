6d2586105ea12d5ef47776f996e5ebb1d450df7f
Arish-Shah <arish.r.shah@gmail.com>
2023-04-02 14:21:11 +0200

    feat: add network policies

--- k8s/api/kube.sh
+++ k8s/api/kube.sh
@@ -2,18 +2,9 @@
 
 dir="$(dirname $0)"
 
-if [[ -z $1 ]]; then
-  microk8s kubectl apply -f $dir/scrolller-api-configmap.yaml
-  microk8s kubectl apply -f $dir/scrolller-api-secret.yaml
-  microk8s kubectl apply -f $dir/scrolller-api-deployment.yaml
-  microk8s kubectl apply -f $dir/scrolller-api-hpa.yaml
-  microk8s kubectl apply -f $dir/scrolller-api-service.yaml
-elif [[ $1 = "u" || $1 = "-u" || $1 = "undo" ]]; then
-  microk8s kubectl delete service scrolller-api-service -n scrolller
-  microk8s kubectl delete hpa scrolller-api-hpa -n scrolller
-  microk8s kubectl delete deployment scrolller-api-deployment -n scrolller
-  microk8s kubectl delete secret scrolller-api-secret -n scrolller
-  microk8s kubectl delete configmaps scrolller-api-configmap -n scrolller
-else
-  echo "usage: ./kube.sh (u/undo)"
-fi
+microk8s kubectl apply -f $dir/scrolller-api-configmap.yaml
+microk8s kubectl apply -f $dir/scrolller-api-secret.yaml
+microk8s kubectl apply -f $dir/scrolller-api-deployment.yaml
+microk8s kubectl apply -f $dir/scrolller-api-hpa.yaml
+microk8s kubectl apply -f $dir/scrolller-api-service.yaml
+microk8s kubectl apply -f $dir/scrolller-api-networkpolicy.yaml
--- k8s/db/kube.sh
+++ k8s/db/kube.sh
@@ -2,19 +2,9 @@
 
 dir="$(dirname $0)"
 
-if [[ -z $1 ]]; then
-  microk8s kubectl apply -f $dir/postgres-configmap.yaml
-  microk8s kubectl apply -f $dir/postgres-secret.yaml
-  microk8s kubectl apply -f $dir/postgres-storage.yaml
-  microk8s kubectl apply -f $dir/postgres-deployment.yaml
-  microk8s kubectl apply -f $dir/postgres-service.yaml
-elif [[ $1 = "u" || $1 = "-u" || $1 = "undo" ]]; then
-  microk8s kubectl delete service postgres-service -n scrolller
-  microk8s kubectl delete deployment postgres-deployment -n scrolller
-  microk8s kubectl delete pvc postgres-pv-claim -n scrolller
-  microk8s kubectl delete pv postgres-pv-volume -n scrolller
-  microk8s kubectl delete secret postgres-secret -n scrolller
-  microk8s kubectl delete configmaps postgres-configmap -n scrolller
-else
-  echo "usage: ./kube.sh (u/undo)"
-fi
+microk8s kubectl apply -f $dir/postgres-configmap.yaml
+microk8s kubectl apply -f $dir/postgres-secret.yaml
+microk8s kubectl apply -f $dir/postgres-storage.yaml
+microk8s kubectl apply -f $dir/postgres-deployment.yaml
+microk8s kubectl apply -f $dir/postgres-service.yaml
+microk8s kubectl apply -f $dir/postgres-networkpolicy.yaml
